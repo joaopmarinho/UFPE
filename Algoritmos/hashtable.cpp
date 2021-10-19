b79cb74f11bc993fe6b4e1243953860eee85c1f3
José Daniel Silva do Carmo <dan321daniel@gmail.com>
2021-10-19 15:04:08 -0300

    1 e 2 funcionando

--- Algoritmos/hashtable.cpp
+++ Algoritmos/hashtable.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
-
 using namespace std;
-
 class Music {
     public:
-        string nome = "None";
+        string nome;
         int tempo = 0;
         int total = 0;
         int key = 0;
@@ -18,15 +16,20 @@ class Music {
 };
 
 int main() {
-    bool a = true;
     int M, t, count = 0;
     string input = "Begin", s;
-    Music *spotify = nullptr;
-
+    Music *spotify = nullptr, *hash = nullptr;
     cin >> M;
+
     spotify = (Music*) calloc(M, sizeof(Music));
     if (spotify == nullptr) {
         cout << "Chorou" << endl;
+        exit(1);
+    }
+
+    hash = (Music*) calloc(500, sizeof(Music));
+    if (hash == nullptr) {
+        cout << "Chorou3" << endl;
         exit(1);
     }
 
@@ -34,52 +37,64 @@ int main() {
         cin >> input;
         if (input == "ADD") {
             count++;
-            if (count-1 > M/2 && a == true) {
-                M = M*2 + 1;
-                spotify = (Music*) realloc(spotify, M * sizeof(Music));
-                if (spotify == nullptr) {
-                    cout << "Chorou2" << endl;
-                    exit(1);
-                }
-                a = false;
-            }
+            bool verif = false;
 
+            if (count-1 > M/2) {
+                M = M*2 + 1;
+                spotify = (Music*) calloc(M, sizeof(Music));
+                if (spotify == nullptr) {
+                    cout << "Chorou2" << endl;  
+                    exit(1);
+                }  
+                for (int i = 0; hash[i].tempo != 0; i++) {
+                    verif = false;
+                    for (int j = 0; verif != true; j++) {
+                        int pos = (hash[i].key + j) % M;
+                        if (spotify[pos].tempo == 0) {
+                            spotify[pos].tempo = t;
+                            verif = true;
+                        } 
+                    }
+                }
+                verif = false;
+            }
             cin >> s >> t;
             int key = 0;
             for (int i = 0; i < s.length() ; i++) {
                 key += s[i]*i;
             }
 
-            bool verif = false;
             for (int i = 0; verif != true; i++) {
                 int pos = (key + i) % M;
+
                 if (spotify[pos].tempo == 0) {
-                    spotify[pos].nome = s;
                     spotify[pos].tempo = t;
-                    spotify[pos].key = key;
-                    verif = true;
+
+                    hash[count-1].nome = s;
+                    hash[count-1].tempo = t;
+                    hash[count-1].key = key;
 
                     cout << s << " " << pos << endl;
+                    verif = true;
                 } 
             }
-
         } else if (input == "PLAY") {
             cin >> s;
             for (int i = 0; i < M; i++) {
-                if (s == spotify[i].nome) {
-                    spotify[i].Increment();
-                    cout << spotify[i].nome << " " << spotify[i].total << endl;
+                if (s == hash[i].nome) {
+                    hash[i].Increment();
+                    cout << hash[i].nome << " " << hash[i].total << endl;
+                    i = M;
                 }
-            }
-            
+            }  
         } else if (input == "TIME") {
             cin >> s;
             for (int i = 0; i < M; i++) {
-                if (s == spotify[i].nome) {
-                    cout << spotify[i].nome << " " << spotify[i].total << endl;
+                if (s == hash[i].nome) {
+                    cout << hash[i].nome << " " << hash[i].total << endl;
+                    i = M;
                 }
             }
-        } else { free(spotify); }
+        } else { free(spotify); free(hash); }
     }
-
 }
