bbde2212ab1158a96b811ec2bd97e66635202bf8
José Daniel Silva do Carmo <dan321daniel@gmail.com>
2021-10-31 01:48:01 -0300

    inicio da atividade, errado

---
+++ Algoritmos/bstTree.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    Node *left;
+    int value;
+    Node *right;
+};
+
+class BST {
+    public:
+        Node *source;
+        Node *target;
+        BST() {
+            source = NULL;
+            target = NULL;
+        }
+        Node* bstInsert(Node *tree, int x) {
+            
+        }
+};
+
+int main() {
+    BST *tree = new BST();
+    int N = 0, x = 0, L = 0, R = 0;
+    bool verif;
+    
+    while (1) {
+        cin >> N;
+        tree->source = (Node*) calloc(N, sizeof(Node));
+        tree->target = (Node*) calloc(N, sizeof(Node));
+
+        for (int i = 0; i < N; i++) {
+            cin >> x;
+            tree->bstInsert(source, x);
+        }
+
+        for (int i = 0; i < N; i++) {
+            cin >> x; 
+            tree->bstInsert(target, x);
+        }
+    }
+}
