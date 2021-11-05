9cd7d5efa3809d16833f7ece337ff60ff8cbccfd
José Daniel Silva do Carmo <dan321daniel@gmail.com>
2021-11-05 15:27:25 -0300

    Começando tudo de novo em BST

--- Algoritmos/bstTree.cpp
+++ Algoritmos/bstTree.cpp
@@ -8,37 +8,20 @@ struct Node {
     Node *right;
 };
 
-class BST {
-    public:
-        Node *source;
-        Node *target;
-        BST() {
-            source = NULL;
-            target = NULL;
-        }
-        Node* bstInsert(Node *tree, int x) {
-            
-        }
-};
-
 int main() {
-    BST *tree = new BST();
+    Node *source, *target;
     int N = 0, x = 0, L = 0, R = 0;
     bool verif;
     
     while (1) {
         cin >> N;
-        tree->source = (Node*) calloc(N, sizeof(Node));
-        tree->target = (Node*) calloc(N, sizeof(Node));
 
         for (int i = 0; i < N; i++) {
             cin >> x;
-            tree->bstInsert(tree->source, x);
         }
 
         for (int i = 0; i < N; i++) {
             cin >> x; 
-            tree->bstInsert(tree->target, x);
         }
     }
 }
