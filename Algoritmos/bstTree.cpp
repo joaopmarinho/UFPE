8154bd03ebe8f9f59b2a6e4a2f1491bea0c14f91
José Daniel Silva do Carmo <dan321daniel@gmail.com>
2021-11-06 22:29:45 -0300

    Atualização lista 3

--- Algoritmos/bstTree.cpp
+++ Algoritmos/bstTree.cpp
@@ -3,10 +3,66 @@
 using namespace std;
 
 struct Node {
-    Node *left;
-    int value;
-    Node *right;
+    Node *left = NULL;
+    int value = 0;
+    Node *right = NULL;
 };
+
+void visit(Node *root) {
+    printf("%d", root->value);
+}
+
+void posOrder(Node *root) {
+    if (root != NULL) {
+        posOrder(root->left);
+        posOrder(root->right);
+        visit(root)
+    }
+}
+
+int height(Node *root) {
+    int HL = 0, HR = 0;
+    if (root != NULL) {
+        HL = height(root->left);
+        HR = height(root->right);
+        return 1+max(HL, HR);
+    }
+}
+
+Node *insert(Node *root, int v) {
+    Node *N = NULL;
+    if (root == NULL) {
+        N = new Node[1];
+        N->value = v;
+        return N;
+    }
+    if (v < root->value) {
+        root->left = insert(root->left, v);
+        return root;
+
+    } else {
+        root->right = insert(root->right, v);
+        return root;
+    }
+} 
+
+Node *left(Node *root) {
+    Node *R, *RL;
+    R = root->right;
+    RL = R->left;
+    R->left = root;
+    root->right = RL;
+    return R;
+}
+
+Node *right(Node *root) {
+    Node *R, *RR;
+    R = root->left;
+    RR = R->right;
+    R->right = root;
+    root->left = RR;
+    return R;
+}
 
 int main() {
     Node *source, *target;
