c7c586374edab06c2338b10df633aee69a41f83f
José Daniel <jdsc2@cin.ufpe.br>
2021-12-13 08:16:37 -0300

    att

--- C/Aula.c
+++ C/Aula.c
@@ -1,78 +1,18 @@
 #include <stdio.h>
-#include <string.h> 
-// Estrutura
-typedef struct{
-  char nome[50];
-  float price;
-} Lista;
+#include <stdlib.h>
 
-int main (){
-  // Declarações
-  int N, cont = 0;
-  float moneyT, auxM, soma = 0;
-  Lista lista[100] = {1}, aux;
-  // Captação de variáveis
-  scanf("%f",&moneyT);
-  scanf("%d", &N);
-  
-  for (int i = 0; i < N; i++) {
-    scanf(" %s %f", lista[i].nome, &lista[i].price);
-    soma += lista[i].price;
-  }
-  // Se ele não conseguir comprar todos os itens:
-  if (moneyT < soma){
-// Ordena a lista por valor 
-//(visto que ele quer pegar o máximo de itens, 
-// vai comprar do menor para o maior).
-    for (int i = 0; i < N-1; i++) {
-      for (int j = i+1; j < N; j++) {
-        if (lista[i].price > lista[j].price) {
-          aux = lista[i];
-          lista[i] = lista[j];
-          lista[j] = aux;
+int main(){
+    int n;
+    while(scanf("%d", &n) > 0){
+        
+        if( n <= 0){
+            exit(1);
         }
-      }
-    } 
-// Um auxiliar para verificação e reutilizar a variável soma
-    auxM = moneyT;
-    soma = 0;
-//Verificando quantos itens ele consegue comprar
-    for (int i = 0; auxM - lista[i].price >= 0; i++) {
-      cont++; //Quantidade de itens
-      auxM = auxM - lista[i].price;
-      soma += lista[i].price; // Dinheiro gasto
-    }
-//Ordenar a lista por nome
-    for (int i = 0; i < cont-1; i++) {
-      for (int j = i+1; j < cont; j++) {
-        if (strcmp(lista[i].nome, lista[j].nome) > 0) {
-          aux = lista[i];
-          lista[i] = lista[j];
-          lista[j] = aux;
+        int *vet = malloc(n * sizeof(int));
+        if(vet != NULL){
+            printf("memoria alocada com sucesso!\n");
         }
-      }
-    } 
-// Imprimir os resultados
-    for (int i = 0; i < cont; i++) {
-      printf("%s %.2f\n", lista[i].nome, lista[i].price);
+        free(vet);
     }
-    printf("%.2f", moneyT - soma);
-
-  } else { // Caso consiga comprar todos os itens:
-  // Ordena a lista por nome
-    for (int i = 0; i < N-1; i++) {
-        for (int j = i+1; j < N; j++) {
-            if (strcmp(lista[i].nome, lista[j].nome) > 0) {
-                aux = lista[i];
-                lista[i] = lista[j];
-                lista[j] = aux;
-            }
-        }
-    } 
-  // Imprime o resultado
-    for (int i = 0; i < N; i++) {   
-      printf("%s %.2f\n", lista[i].nome, lista[i].price);
-    }
-    printf("%.2f",moneyT - soma);
-  }
+    return 0;
 }
--- C/MP.c
+++ C/MP.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 // jdsc2
-
+// Estruturas:
 typedef struct {
     char nome[20], mat[10];
     float notas[5];
@@ -16,11 +16,10 @@ typedef struct {
     char professor[20], id[5];
     Aluno *alunos;
 } Turma;
-
+// Algoritmo de ordenação:
 void selection_sort(Aluno arr[], int size) {
     int i;
-    for (i = 0; i < (size-1); i++) 
-    {
+    for (i = 0; i < (size-1); i++) {
         int min = i; 
         //min pois no próximo for o i será +1 para que passe a verificar os próximos
         int j;
@@ -39,87 +38,95 @@ void selection_sort(Aluno arr[], int size) {
         }
     }
 }
-
-void inserir(Turma **turmas, int i) {
+// Funçao 1. Inserção:
+void inserir(Turma *turmas, int i) {
     int quantidade, falta, j, k;
     char id[5], nome[20], matricula[10], teacher[20];
     float nota[5];
 
-    (*turmas) = (Turma *) realloc(*turmas, i*sizeof(Turma));
-    if (*turmas == NULL) {
+    // Atualizar o vetor dinâmico de turmas
+    turmas = (Turma *) realloc(turmas, i*sizeof(Turma));
+    if (turmas == NULL) {
         printf("Erro ao criar turma");
         exit(1);
     }
-    
+    // Capaz de ler uma turma do teclado
     printf("Quantidade de alunos:\n");
     scanf("%d", &quantidade);
-    (*turmas)[i-1].qtd = quantidade;
+    turmas[i-1].qtd = quantidade;
     
     printf("Professor:\n");
     scanf(" %s", teacher);
-    strcpy((*turmas)[i-1].professor, teacher);
+    strcpy(turmas[i-1].professor, teacher);
 
     printf("ID\n");
     scanf(" %s", id);
-    strcpy((*turmas)[i-1].id, id);
-    
-    (*turmas)[i-1].alunos = (Aluno *) calloc((*turmas)[i-1].qtd, sizeof(Aluno));
+    strcpy(turmas[i-1].id, id);
+    // Alocar os alunos dinamicamente
+    turmas[i-1].alunos = (Aluno *) calloc(turmas[i-1].qtd, sizeof(Aluno));
 
     for (j = 0; j < quantidade; j++) {
-
+    // Leitura deve acabar quando a quantidade de alunos for igual a 0.
         printf("Nome do aluno:\n");
         scanf(" %s", nome);
-        strcpy((*turmas)[i-1].alunos[j].nome, nome);
+        strcpy(turmas[i-1].alunos[j].nome, nome);
 
         printf("Matricula:\n");
         scanf(" %s", matricula);
-        strcpy((*turmas)[i-1].alunos[j].mat, matricula);
+        strcpy(turmas[i-1].alunos[j].mat, matricula);
 
         printf("Notas:\n");
         for (k = 0; k < 5; k++) {
             scanf("%f", &nota[k]);
-            (*turmas)[i-1].alunos[j].notas[k] = nota[k];
-            (*turmas)[i-1].alunos[j].media += nota[k];
+            turmas[i-1].alunos[j].notas[k] = nota[k];
+            turmas[i-1].alunos[j].media += nota[k];
         }
 
         printf("N de faltas\n");
         scanf("%d", &falta);
-        (*turmas)[i-1].alunos[j].faltas = falta;
+        turmas[i-1].alunos[j].faltas = falta;
     }
-
-    selection_sort((*turmas)[i-1].alunos, (*turmas)[i-1].qtd);
+// Ordenar os alunos em ordem afabética! Importante para o caso 5. Turmas
+    selection_sort(turmas[i-1].alunos, turmas[i-1].qtd);
 }
-
+// Função Remoção de aluno
 void excluir(Turma *turmas, int i) {
     char nome[20], matricula[20];
     int j, k;
-
+    // Capaz de ler o nome e matrícula do teclado
     printf("Escolha o nome:\n");
     scanf(" %s", nome);
 
     printf("matricula para exclusao:\n");
     scanf(" %s", matricula);
-
+    // Procurar no vetor de turmas 
     for (j = 0; j < i-1; j++) {
         for (k = 0; k < turmas[j].qtd; k++) {
+            // Remover onde estiver
             if (strcmp(nome, turmas[j].alunos[k].nome) == 0 && strcmp(matricula, turmas[j].alunos[k].mat) == 0) {
+            // Troca o aluno que será excluído pelo último aluno da turma
                 turmas[j].alunos[k] = turmas[j].alunos[turmas[j].qtd-1];
+            // Diminui a quantidade de alunos da turma
                 turmas[j].qtd -= 1;
+            // Realoca o vetor para -1 do que era antes.
+            //Deixando de lado o aluno a ser excluído.
                 turmas[j].alunos = (Aluno *) realloc(turmas[j].alunos, turmas[j].qtd * sizeof(Aluno));
             }
         }
     }
 }
-
+// Função Aprovados
 void aprovado(Turma *turmas, int i) {
     char id[5];
     int j, k;
-
+// Ler o ID de uma turma
     printf("Diga o ID da turma\n");
     scanf(" %s", id);
     for (j = 0; j < i-1; j++) {
+        // Procura a turma pelo ID
         if (strcmp(id, turmas[j].id) == 0) {
             for (k = 0; k < turmas[j].qtd; k++) {
+            // Buscar todos os alunos com média >= 7
                 if (turmas[j].alunos[k].media/5 >= 7) {
                     printf("Aluno %d: %s\n", k+1, turmas[j].alunos[k].nome);
                 }
@@ -127,16 +134,19 @@ void aprovado(Turma *turmas, int i) {
         }
     }
 }
-
+// Função reprovados
 void reprovado(Turma *turmas, int i) {
     int j, k;
     char id[5];
-
+// Ler o ID de uma turma:
     printf("Diga o ID da turma\n");
     scanf(" %s", id);
+
     for (j = 0; j < i-1; j++) {
+        // Procura a turma pelo ID
         if (strcmp(id, turmas[j].id) == 0) {
             for (k = 0; k < turmas[j].qtd; k++) {
+            // Buscar todos os alunos com média < 3
                 if (turmas[j].alunos[k].media/5 < 3) {
                     printf("Aluno %d°: %s\n", k+1, turmas[j].alunos[k].nome);
                 }
@@ -144,20 +154,21 @@ void reprovado(Turma *turmas, int i) {
         }
     }
 }
-
+// Função Turmas:
 void exibir(Turma *turmas, int i) {
     int j, k;
-    
     for (j = 0; j < i-1; j++) {
+    // Exibe na tela o nome do professor de cada turma
         printf("* Turma %d: %s\n", j+1, turmas[j].professor);
         for (k = 0; k < turmas[j].qtd; k++) {
+            // Exibir os alunos em ordem alfabética:
             printf("- %s\n", turmas[j].alunos[k].nome);
         }
         printf("--------------------------------------\n");
     }
 }
 
-int main (void) { 
+int main () { 
     int verif = 1, menu, i = 1, j;
 
     Turma *turmas;
@@ -167,11 +178,12 @@ int main (void) {
         printf("Escolha entre 1-5\n");
         scanf("%d", &menu);
         //scr sei de mais nada
-        switch (menu)
-        {
-        case 1:
-            inserir(&turmas, i);
+        switch (menu) { 
+        case 1: 
+            inserir(turmas, i);
+            // Adicionar +1 número de turmas
             i++;
+//Poderia colocar isso na função passando como argumento &i
             break;
         case 2:
             excluir(turmas, i);
@@ -190,11 +202,14 @@ int main (void) {
             verif = 0;
             break;
         }
-    }
+    } 
     printf("Saiu do programa, free\n");
+    // Desalocar os vetores 
     for (j = 0; j < i; j++) {
+    // Desalocar cada vetor 'alunos' de cada turma
         free(turmas[j].alunos);
     }
+    // Desalocar o turmas.
     free(turmas);
 }
 
